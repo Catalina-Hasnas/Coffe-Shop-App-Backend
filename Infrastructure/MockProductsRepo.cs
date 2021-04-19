d4f420a39ff919a68ad981d5f19c6cd978749baf
Catalina Hasnas <catalinahasnas1996@gmail.com>
2021-04-19 15:18:53 +0300

    fixed GedProductById()

--- Infrastructure/MockProductsRepo.cs
+++ Infrastructure/MockProductsRepo.cs
@@ -8,28 +8,21 @@ namespace Infrastructure
 {
     public class MockProductsRepo : IProductsRepo
     {
-        public IEnumerable<Product> GetAllProducts()
+        private List<Product> _products = new List<Product>
         {
-            var products = new List<Product>
-            {
             new Product { Id = 1, Amount = 5, Image = "https://via.placeholder.com/320x280", Price = 200, PriceFormatted = "200$", Title = "First Product", CreatedAt = DateTime.Now },
             new Product { Id = 2, Amount = 6, Image = "https://via.placeholder.com/320x280", Price = 210, PriceFormatted = "210$", Title = "Second Product", CreatedAt = DateTime.Now  },
             new Product { Id = 3, Amount = 6, Image = "https://via.placeholder.com/320x280", Price = 220, PriceFormatted = "220$", Title = "Third Product", CreatedAt = DateTime.Now  }
             };
-
-            return products;
+    
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return _products;
         }
 
         public Product GetProductById(int id)
         {
-            var products = new List<Product>
-            {
-            new Product { Id = 1, Amount = 5, Image = "https://via.placeholder.com/320x280", Price = 200, PriceFormatted = "200$", Title = "First Product", CreatedAt = DateTime.Now },
-            new Product { Id = 2, Amount = 6, Image = "https://via.placeholder.com/320x280", Price = 210, PriceFormatted = "210$", Title = "Second Product", CreatedAt = DateTime.Now  },
-            new Product { Id = 3, Amount = 6, Image = "https://via.placeholder.com/320x280", Price = 220, PriceFormatted = "220$", Title = "Third Product", CreatedAt = DateTime.Now  }
-            };
-
-            return products.FirstOrDefault(p => p.Id == id);
+            return _products.FirstOrDefault(p => p.Id == id);
         }
     }
 }
