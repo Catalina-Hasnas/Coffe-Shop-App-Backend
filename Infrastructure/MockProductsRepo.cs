8e0c1cf9d03d9e01c4e77a7b939e550c62ec374b
Catalina Hasnas <catalinahasnas1996@gmail.com>
2021-04-19 14:00:01 +0300

    added getProductsById mock implementation

--- Infrastructure/MockProductsRepo.cs
+++ Infrastructure/MockProductsRepo.cs
@@ -2,6 +2,7 @@
 using Domain;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Infrastructure
 {
@@ -21,9 +22,14 @@ namespace Infrastructure
 
         public Product GetProductById(int id)
         {
-            // to implement
-            // extension method singleOrDefault
-            return new Product { Id = 1, Amount = 5, Image = "https://via.placeholder.com/320x280", Price = 200, PriceFormatted = "200$", Title = "First Product", CreatedAt = DateTime.Now };
+            var products = new List<Product>
+            {
+            new Product { Id = 1, Amount = 5, Image = "https://via.placeholder.com/320x280", Price = 200, PriceFormatted = "200$", Title = "First Product", CreatedAt = DateTime.Now },
+            new Product { Id = 2, Amount = 6, Image = "https://via.placeholder.com/320x280", Price = 210, PriceFormatted = "210$", Title = "Second Product", CreatedAt = DateTime.Now  },
+            new Product { Id = 3, Amount = 6, Image = "https://via.placeholder.com/320x280", Price = 220, PriceFormatted = "220$", Title = "Third Product", CreatedAt = DateTime.Now  }
+            };
+
+            return products.FirstOrDefault(p => p.Id == id);
         }
     }
 }
