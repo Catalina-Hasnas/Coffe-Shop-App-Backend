4fe87be0aa705c1d5665e19f83704b41a7ea4e28
Catalina Hasnas <catalinahasnas1996@gmail.com>
2021-04-29 18:35:10 +0300

    added DTOs

---
+++ Application/DtoModels/ProductDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DtoModels
+{
+    public class ProductDto
+    {       
+        public int Id { get; set; }
+        public int Amount { get; set; }
+        public string Image { get; set; }
+        public int Price { get; set; }       
+        public string Title { get; set; }       
+        public DateTime CreatedAt { get; set; }
+        public CategoryDto Category { get; set; }
+    }
+}
