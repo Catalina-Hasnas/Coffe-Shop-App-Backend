c04f4b3d01e0ac31b7ae10fe5c078bd4b5aaedff
Catalina Hasnas <catalinahasnas1996@gmail.com>
2021-05-09 18:02:19 +0300

    added GetAllProductsWithPromotion logic

--- Infrastructure/Repositories/ProductsRepo.cs
+++ Infrastructure/Repositories/ProductsRepo.cs
@@ -50,6 +50,74 @@ namespace Infrastructure.Repositories
         {
             await _context.SaveChangesAsync();
         }
+
+        public async Task<IEnumerable<Product>> GetAllProductsWithPromotion()
+        {
+            var categoryInfo = _context.Products
+                        .Include(p => p.Category)
+                        .Include(p => p.Promotion)
+                        .GroupBy(x => x.CategoryId)
+                        .Select(x => new
+                        {
+                            Name = x.Key,
+                            AveragePrice = x.Average(y => y.Price),
+                            ProductCount = x.Count()
+                        })
+                        .Where(x => x.ProductCount >= 1)
+                        .ToList();
+
+            Console.WriteLine(categoryInfo);
+
+            var categoryInfo2 = _context.Categories
+                        .Include(c => c.Products)
+                        .ToList()
+                        .GroupJoin(
+                            _context.Products,
+                            c => c.Id,
+                            p => p.CategoryId,
+                            (category, products) => new
+                            {
+                                Name = category.Name,
+                                AveragePrice = products.Average(x => x.Price),
+                                ProductCount = products.Count(),
+                                ProductNames = products.Select(p => p.Title).ToList()
+                            }
+                          ).ToList();
+
+            Console.WriteLine(categoryInfo2);
+
+            var categoryInfo3 = _context.Categories
+                            .Include(c => c.Products)
+                            .Where(c => c.Products.All(p => p.Promotion == null))
+                            .ToList();
+            Console.WriteLine(categoryInfo3);
+
+            var categoryInfo4 = _context.Categories
+                            .Include(c => c.Products)
+                            .Where(c => c.Products.Any(p => p.Promotion != null))
+                            .ToList();
+            Console.WriteLine(categoryInfo4);
+
+            var productPriceInfo5 = _context.Products
+                            .Include(p => p.Promotion)
+                            .Select(p => new
+                            {
+                                OriginalPrice = p.Price,
+                                RealPrice = p.Promotion == null ? p.Price : p.Price - p.Promotion.Discount,
+                                ProductId = p.Id
+
+                            })
+                            .ToList();
+
+            Console.WriteLine(productPriceInfo5);
+
+
+            return await _context.Products
+                        .Include(p => p.Category)
+                        .Include(p => p.Promotion)
+                        .Where(p => p.Promotion != null)
+                        .ToListAsync();
+        }
     }
 }
 
