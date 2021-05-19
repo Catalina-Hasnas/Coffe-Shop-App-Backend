5183db482f80a00c8bec6daf352ea9c90253e535
Catalina Hasnas <catalinahasnas1996@gmail.com>
2021-05-19 12:28:52 +0300

    added updateProduct logic

---
+++ Application/Commands/ProductCommands/UpdateProductCommand.cs
@@ -0,0 +1,20 @@
+﻿using Application.DtoModels;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Commands.ProductCommands
+{
+    public class UpdateProductCommand : IRequest<ProductDto>
+    {
+        public int Id { get; set; }
+        public int Amount { get; set; }
+        public string Image { get; set; }
+        public float Price { get; set; }
+        public string Title { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
